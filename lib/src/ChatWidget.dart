de52f5348ff7878ec6677d37e1949256e1623005
RaheemBureyII <94723743+RaheemBureyII@users.noreply.github.com>
2022-09-26 14:39:26 -0500

    BugFix#1

--- lib/src/ChatWidget.dart
+++ lib/src/ChatWidget.dart
@@ -49,8 +49,56 @@ class _ChatWidgetState extends State<ChatWidget> {
                 androidOnPermissionRequest: (controller, origin, resources) async {
                   return PermissionRequestResponse(resources: resources, action: PermissionRequestResponseAction.GRANT);
                 },
-                onLoadStop: (controller,url)async{
-                  var result = await controller.injectJavascriptFileFromAsset(assetFilePath: "package:fluttercrisp/assets/index.js");
+                 onLoadStop: (controller,url)async{
+                  final String functionBody = """
+                var jsondata;
+                      var webid;
+                      window.addEventListener("flutterInAppWebViewPlatformReady",  function(event) {
+                       const args = ["we in there like swimwear"];
+                       window.flutter_inappwebview.callHandler('myHandlerName', ...args).then(function(result) {
+                                     jsondata=JSON.parse(result);
+                                      webid=jsondata.webid;
+                                   });;
+                      });
+                      window.CRISP_RUNTIME_CONFIG = {
+                        lock_maximized       : true,
+                        cross_origin_cookies : true
+                      };
+                      function onagenmessage(message){
+                      const args = [message.content];
+                          window.flutter_inappwebview.callHandler('agentsentmessage',...args);
+                      }
+                      function onstart(){
+                      const args = ["started"];
+                          window.flutter_inappwebview.callHandler('onstart',...args);
+                      }
+                      
+                      function loadup(){
+                      onstart();
+                       window.\$crisp=[];window.CRISP_WEBSITE_ID="1fe61c88-a23f-40f2-aa2b-1e4a554edcde";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
+                      }
+                       window.CRISP_READY_TRIGGER =function(){
+                          if(\$crisp.is("chat:opened")===true){
+                          \$crisp.push(["set", "user:email", [jsondata.email]]);
+                          \$crisp.push(["set", "user:nickname", [jsondata.name]]);
+                          \$crisp.push(["set", "user:company", [jsondata.company]]);
+                          \$crisp.push(["on", "message:received", onagenmessage])
+                          }
+                       }
+                       loadup();
+                       //window.\$crisp=[];window.CRISP_WEBSITE_ID="1fe61c88-a23f-40f2-aa2b-1e4a554edcde";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
+                      //document.onreadystatechange = () => {
+                      //  if (document.readyState === 'complete') {
+                      //    setTimeout(loadup,200);
+                      //    //window.\$crisp=[];window.CRISP_WEBSITE_ID=jsondata.webid;(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
+                      //
+                      //  }
+                      //};
+
+
+                  """;
+                  var result = await controller.callAsyncJavaScript(
+                      functionBody: functionBody);
                 },
                 onWebViewCreated: (controller){
                   controller.addJavaScriptHandler(handlerName: 'UploadInfo', callback: (args) async {
