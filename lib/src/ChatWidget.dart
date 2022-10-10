3317c2cf414089bef4e23e8a3642bfc3a8b536fd
RaheemBureyII <raheemburey2.0.1@gmail.com>
2022-10-10 16:36:56 -0500

    Added Headless mode

--- lib/src/ChatWidget.dart
+++ lib/src/ChatWidget.dart
@@ -1,13 +1,15 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 class ChatWidget extends StatefulWidget {
-  ChatWidget({Key? key, this.backgroundColor, this.name, this.company, this.email, required this.webid,this.onLoad, this.onAgentMessage,this.sessiondata,required this.website}) : super(key: key);
+  ChatWidget({Key? key, this.backgroundColor,this.phone, this.name, this.company, this.email, required this.webid,this.onLoad, this.onAgentMessage,this.sessiondata,required this.website}) : super(key: key);
   final  backgroundColor;
   final name;
   final company;
+  final phone;
   final email;
   final webid;
   final website;
@@ -19,6 +21,60 @@ class ChatWidget extends StatefulWidget {
 }
 
 class _ChatWidgetState extends State<ChatWidget> {
+  late HeadlessInAppWebView headlessInAppWebView;
+  @override
+  void initState(){
+    headlessInAppWebView=HeadlessInAppWebView(
+      initialOptions: InAppWebViewGroupOptions(
+          crossPlatform: InAppWebViewOptions(
+              preferredContentMode: UserPreferredContentMode.MOBILE,
+              mediaPlaybackRequiresUserGesture: false,
+              disableHorizontalScroll: true,
+              disableVerticalScroll: true
+
+          ),
+          android: AndroidInAppWebViewOptions(
+              useHybridComposition: true
+          ),
+          ios: IOSInAppWebViewOptions(
+            allowsInlineMediaPlayback: true,
+
+            //allowsInlineMediaPlayback: true,
+          )
+      ),
+      initialUrlRequest: URLRequest(url: Uri.parse(widget?.website)),
+      androidOnPermissionRequest: (controller, origin, resources) async {
+        return PermissionRequestResponse(resources: resources, action: PermissionRequestResponseAction.GRANT);
+      },
+      onLoadStart: (controller,url)async{
+        final String functionBody1 = function;
+        var result = await controller.callAsyncJavaScript(
+            functionBody: functionBody1);
+      },
+      onWebViewCreated: (controller){
+        controller.addJavaScriptHandler(handlerName: 'UploadInfo', callback: (args) async {
+          print("in there");
+          Map<String,dynamic> data={
+            "company":widget.company,
+            "webid":widget.webid,
+            "name":widget.name,
+            "email":widget.email,
+            "phone":widget.phone,
+            "session_data":widget.sessiondata
+          };
+          final json = jsonEncode(data);
+          return json;
+        });
+        controller.addJavaScriptHandler(handlerName: 'onLoad', callback: (args) async {
+          widget.onLoad?.call();
+        });
+        controller.addJavaScriptHandler(handlerName: 'onAgentMessage', callback: (args) async {
+          widget.onAgentMessage?.call();
+        });
+      },
+    );
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
 
@@ -28,30 +84,73 @@ class _ChatWidgetState extends State<ChatWidget> {
           child: Center(
             child: Container(
               width: MediaQuery.of(context).size.width>559?559:double.infinity,
-              child: InAppWebView(
-                initialOptions: InAppWebViewGroupOptions(
-                    crossPlatform: InAppWebViewOptions(
-                        preferredContentMode: UserPreferredContentMode.MOBILE,
-                        mediaPlaybackRequiresUserGesture: false,
-                        disableHorizontalScroll: true,
-                        disableVerticalScroll: true
+              child: Stack(
+                children: [
+                  InAppWebView(
+                    initialOptions: InAppWebViewGroupOptions(
+                        crossPlatform: InAppWebViewOptions(
+                            preferredContentMode: UserPreferredContentMode.MOBILE,
+                            mediaPlaybackRequiresUserGesture: false,
+                            disableHorizontalScroll: true,
+                            disableVerticalScroll: true
 
-                    ),
-                    android: AndroidInAppWebViewOptions(
-                        useHybridComposition: true
-                    ),
-                    ios: IOSInAppWebViewOptions(
-                      allowsInlineMediaPlayback: true,
+                        ),
+                        android: AndroidInAppWebViewOptions(
+                            useHybridComposition: true
+                        ),
+                        ios: IOSInAppWebViewOptions(
+                          allowsInlineMediaPlayback: true,
 
-                      //allowsInlineMediaPlayback: true,
-                    )
-                ),
-                initialUrlRequest: URLRequest(url: Uri.parse(widget?.website)),
-                androidOnPermissionRequest: (controller, origin, resources) async {
-                  return PermissionRequestResponse(resources: resources, action: PermissionRequestResponseAction.GRANT);
-                },
-                onLoadStart: (controller,url)async{
-                  final String functionBody1 = """
+                          //allowsInlineMediaPlayback: true,
+                        )
+                    ),
+                    initialUrlRequest: URLRequest(url: Uri.parse(widget?.website)),
+                    androidOnPermissionRequest: (controller, origin, resources) async {
+                      return PermissionRequestResponse(resources: resources, action: PermissionRequestResponseAction.GRANT);
+                    },
+                    onLoadStart: (controller,url)async{
+                      final String functionBody1 = function;
+                      var result = await controller.callAsyncJavaScript(
+                          functionBody: functionBody1);
+                    },
+                    onWebViewCreated: (controller){
+                      controller.addJavaScriptHandler(handlerName: 'UploadInfo', callback: (args) async {
+                        print("in there");
+                        Map<String,dynamic> data={
+                          "company":widget.company,
+                          "webid":widget.webid,
+                          "name":widget.name,
+                          "email":widget.email,
+                          "phone":widget.phone,
+                          "session_data":widget.sessiondata
+                        };
+                        final json = jsonEncode(data);
+                        return json;
+                      });
+                      controller.addJavaScriptHandler(handlerName: 'onLoad', callback: (args) async {
+                        widget.onLoad?.call();
+                      });
+                    },
+                  ),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children:[ IconButton(onPressed: (){
+
+                        Get.back();
+                        headlessInAppWebView.run();
+                        //changestate(false);
+                      }
+                          , icon: Icon(Icons.close_outlined,color: Theme.of(context).primaryColor,size: MediaQuery.of(context).size.width*0.06,))])
+                ],
+              ),
+            ),
+          ),
+        )
+    );
+  }
+}
+
+String function ="""
                   const style = document.createElement('style');
 
                   style.textContent = `
@@ -116,7 +215,7 @@ class _ChatWidgetState extends State<ChatWidget> {
                       };
                       function onagenmessage(message){
                       const args = [message.content];
-                          window.flutter_inappwebview.callHandler('agentsentmessage',...args);
+                          window.flutter_inappwebview.callHandler('onAgentMessage',...args);
                       }
                       function onstart(){
                       const args = ["started"];
@@ -130,9 +229,9 @@ class _ChatWidgetState extends State<ChatWidget> {
                        window.CRISP_READY_TRIGGER =function(){
                        //alert("yurr");
                           if(\$crisp.is("chat:opened")===true){
-                           alert("yurr1");
                           \$crisp.push(["set", "user:email", [jsondata.email]]);
                           \$crisp.push(["set", "user:nickname", [jsondata.name]]);
+                          \$crisp.push(["set", "user:phone", [jsondata.phone]]);
                           \$crisp.push(["set", "user:company", [jsondata.company]]);
                           \$crisp.push(["set", "session:data", [Object.entries(jsondata.session_data)]]);
                           \$crisp.push(["on", "message:received", onagenmessage])
@@ -150,33 +249,3 @@ class _ChatWidgetState extends State<ChatWidget> {
                       // };
 
                   """;
-                  var result = await controller.callAsyncJavaScript(
-                      functionBody: functionBody1);
-                },
-                onWebViewCreated: (controller){
-                  controller.addJavaScriptHandler(handlerName: 'UploadInfo', callback: (args) async {
-                    print("in there");
-                    Map<String,dynamic> data={
-                      "company":widget.company,
-                      "webid":widget.webid,
-                      "name":widget.name,
-                      "email":widget.email,
-                      "session_data":widget.sessiondata
-                    };
-                    final json = jsonEncode(data);
-                    return json;
-                  });
-                  controller.addJavaScriptHandler(handlerName: 'onLoad', callback: (args) async {
-                    widget.onLoad?.call();
-                  });
-                  controller.addJavaScriptHandler(handlerName: 'onAgentMessage', callback: (args) async {
-                    widget.onAgentMessage?.call();
-                  });
-                },
-              ),
-            ),
-          ),
-        )
-    );
-  }
-}
